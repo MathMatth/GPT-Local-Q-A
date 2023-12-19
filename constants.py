2d4197ea0fa247acc9f8d5a6999c4820d651aa0c
MathMatth <150814077+MathMatth@users.noreply.github.com>
2023-12-20 00:38:01 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,3 +6,8 @@ EMB_SBERT_MINILM = "sentence-transformers/all-MiniLM-L6-v2" # Chroma takes care 
 
 LLM_FLAN_T5_BASE = "google/flan-t5-base"
 LLM_MISTRAL = "mistralai/Mistral-7B-v0.1"
+LLM_BLOOM = "bigscience/bloom"
+LLM_FALCON = "tiiuae/falcon-7b"
+LLM_VICUNA = "lmsys/vicuna-7b-v1.5"
+LLM_GPT2  = "gpt2"
+
